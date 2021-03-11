b9538603d0c58154565f5d49c7430f5d2e4369d2
Алексей <alexleskov2000@gmail.com>
2021-03-11 21:05:05 +0800

    Создать модель для обратной связи

--- request_manager/admin.py
+++ request_manager/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from request_manager import models
+
+
+@admin.register(models.RequestStatus)
+class RequestStatusAdmin(admin.ModelAdmin):
+    list_display = ('status',)
+    ordering = ['-update_date']
+
+
+@admin.register(models.RequestHistory)
+class RequestHistoryAdmin(admin.ModelAdmin):
+    list_display = ('status', 'editor', 'tg_user', 'vk_user')
+    search_fields = ('editor', 'tg_user', 'vk_user')
+    ordering = ['-update_date']
