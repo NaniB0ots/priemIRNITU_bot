a53a353a95ba58876b41280216e9aac6ef39f8e1
Алексей <alexleskov2000@gmail.com>
2021-03-11 20:33:18 +0800

    Создать модель для категорий

--- question_manager/admin.py
+++ question_manager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from question_manager import models
+
+
+@admin.register(models.Category)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('category', 'parent_category_id', 'editor', 'contains_questions')
+    search_fields = ('category',)
+    list_filter = ('contains_questions',)
+    ordering = ['-update_date']
--- question_manager/models.py
+++ question_manager/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    parent_category_id = models.IntegerField(null=True, blank=True, verbose_name='Id родительской категории')
+    editor = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Кто создал')
+    contains_questions = models.BooleanField(default=True, verbose_name='Содержит вопросы (иначе категории)')
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['-update_date']
+
+    def __str__(self):
+        return f'{self.category}'
