0330c6279babf718a759260af7652d7ceaebc7f5
Алексей <alexleskov2000@gmail.com>
2021-03-11 20:41:04 +0800

    Создать модель для вопросов

--- question_manager/admin.py
+++ question_manager/admin.py
@@ -4,8 +4,16 @@ from question_manager import models
 
 
 @admin.register(models.Category)
-class ProjectAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('category', 'parent_category_id', 'editor', 'contains_questions')
     search_fields = ('category',)
     list_filter = ('contains_questions',)
+    ordering = ['-update_date']
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer', 'category', 'editor')
+    search_fields = ('question', 'answer')
+    list_filter = ('category',)
     ordering = ['-update_date']
--- question_manager/models.py
+++ question_manager/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Category(models.Model):
     category = models.CharField(max_length=100, verbose_name='Категория')
     parent_category_id = models.IntegerField(null=True, blank=True, verbose_name='Id родительской категории')
-    editor = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Кто создал')
+    editor = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Кто изменил')
     contains_questions = models.BooleanField(default=True, verbose_name='Содержит вопросы (иначе категории)')
 
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -18,3 +18,21 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.category}'
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=100, verbose_name='Вопрос')
+    answer = models.CharField(max_length=195, verbose_name='Ответ')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    editor = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Кто изменил')
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+        ordering = ['-update_date']
+
+    def __str__(self):
+        return f'{self.question}'
