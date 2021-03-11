2f32e2517c7e5898f41463358eac680af4cbb42b
Алексей <alexleskov2000@gmail.com>
2021-03-11 19:56:18 +0800

    Выполнить миограции

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'question_manager'
+    'question_manager',
     'tg_bot',
     'vk_bot',
 ]
