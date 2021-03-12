b798c240104c9cbdf982a2dc94e4f54c312b0209
Алексей <alexleskov2000@gmail.com>
2021-03-12 18:08:29 +0800

    Изменить порядок запуска ботов

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from threading import Thread
+from tg_bot.tg_bot import bot as tg_bot
+from vk_bot.vk_bot import bot as vk_bot
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# запуск чат-ботов
+thread_tg = Thread(target=tg_bot.infinity_polling)
+thread_tg.start()
+thread_vk = Thread(target=vk_bot.run_forever)
+thread_vk.start()
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,10 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from threading import Thread
-from tg_bot.tg_bot import bot as tg_bot
-from vk_bot.vk_bot import bot as vk_bot
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
 
-# запуск чат-ботов
-thread_tg = Thread(target=tg_bot.infinity_polling)
-thread_tg.start()
-thread_vk = Thread(target=vk_bot.run_forever)
-thread_vk.start()
+
