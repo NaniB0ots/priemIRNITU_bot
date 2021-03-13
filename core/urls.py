7ffbfc668d0bd468c29be8305b9222aeb70d7592
Алексей <alexleskov2000@gmail.com>
2021-03-13 14:29:58 +0800

    запуск telegram бота manage командой

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from threading import Thread
-from tg_bot.tg_bot import bot as tg_bot
 from vk_bot.vk_bot import bot as vk_bot
 
 urlpatterns = [
@@ -9,7 +8,5 @@ urlpatterns = [
 ]
 
 # запуск чат-ботов
-thread_tg = Thread(target=tg_bot.infinity_polling)
-thread_tg.start()
-thread_vk = Thread(target=vk_bot.run_forever)
-thread_vk.start()
+# thread_vk = Thread(target=vk_bot.run_forever)
+# thread_vk.start()
