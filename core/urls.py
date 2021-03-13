4a50a4e332e502bb48e8cf505150851f1bfe496d
Алексей <alexleskov2000@gmail.com>
2021-03-13 15:31:32 +0800

    Запуск Django в Docker

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from threading import Thread
-from vk_bot.vk_bot import bot as vk_bot
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-# запуск чат-ботов
-# thread_vk = Thread(target=vk_bot.run_forever)
-# thread_vk.start()
