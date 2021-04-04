129dd9677059e5bb8b43c29c9e0203f61b0c023d
Алексей <alexleskov2000@gmail.com>
2021-04-04 11:21:18 +0800

    Вывод дерева категорий

--- web/project/urls.py
+++ web/project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('question_manager/', include('question_manager.urls'))
 ]
--- web/question_manager/views.py
+++ web/question_manager/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from question_manager import models
 
-# Create your views here.
+
+def show_categories(request):
+    return render(request, 'question_manager/category_list.html',
+                  {'categories': models.Category.objects.all()})
