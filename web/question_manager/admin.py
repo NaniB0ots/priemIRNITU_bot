66c2ae8dc945fb91f08d77bdaa3762d3c658ba78
Алексей <alexleskov2000@gmail.com>
2021-04-04 12:30:10 +0800

    Возможность перетаскивать категории в админке

--- web/question_manager/admin.py
+++ web/question_manager/admin.py
@@ -1,21 +1,36 @@
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
+
+from mptt.admin import DraggableMPTTAdmin
 
 from question_manager import models
 
 
+class QuestionInline(admin.TabularInline):
+    model = models.Question
+    show_change_link = True
+    extra = 0
+
+
 @admin.register(models.Category)
-class CategoryAdmin(MPTTModelAdmin):
-    # list_display = ('category', 'parent_category', 'editor', 'contains_questions')
-    # search_fields = ('category',)
-    # list_filter = ('contains_questions',)
-    mptt_level_indent = 20
-    # ordering = ['-update_date']
+class CategoryAdmin(DraggableMPTTAdmin):
+    inlines = [
+        QuestionInline,
+    ]
+    mptt_level_indent = 30
+    list_display = ('tree_actions', 'indented_title',)
+    list_display_links = ('indented_title',)
+
+    def get_object(self, *args, **kwargs):
+        category = super(CategoryAdmin, self).get_object(*args, **kwargs)
+        # Если у категории есть подкатегории убираем вложенные сообщения.
+        if models.Category.objects.filter(parent=category):
+            self.inlines = []
+        return category
 
 
 @admin.register(models.Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer', 'category', 'editor')
+    list_display = ('question', 'answer', 'category')
     search_fields = ('question', 'answer')
     list_filter = ('category',)
     ordering = ['-update_date']
--- web/question_manager/models.py
+++ web/question_manager/models.py
@@ -5,18 +5,15 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(MPTTModel):
     category = models.CharField(max_length=37, unique=True, verbose_name='Категория')
-    editor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
-                               verbose_name='Кто изменил')
-    contains_questions = models.BooleanField(default=True, verbose_name='Содержит вопросы (иначе категории)')
 
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,
-                                     related_name='children', verbose_name='Родительская категория')
+                            related_name='children', verbose_name='Родительская категория')
 
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = 'Категория'
+        verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
 
     class MPTTMeta:
@@ -29,8 +26,7 @@ class Category(MPTTModel):
 class Question(models.Model):
     question = models.TextField(verbose_name='Вопрос')
     answer = models.TextField(verbose_name='Ответ')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
-    editor = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Кто изменил')
+    category = TreeForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
 
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
