3a8d6bccb23cf1d5e11a99fd622a86bbb2b74018
Алексей <alexleskov2000@gmail.com>
2021-04-12 20:38:20 +0800

    Функция создания запроса обратной связи

--- web/request_manager/admin.py
+++ web/request_manager/admin.py
@@ -3,19 +3,12 @@ from django.contrib import admin
 from request_manager import models
 
 
-@admin.register(models.RequestStatus)
-class RequestStatusAdmin(admin.ModelAdmin):
-    list_display = ('status',)
-    ordering = ['-update_date']
-
-
 @admin.register(models.RequestHistory)
 class RequestHistoryAdmin(admin.ModelAdmin):
-    list_display = ('status', 'editor', 'tg_user', 'vk_user')
-    search_fields = ('editor', 'tg_user', 'vk_user')
+    list_display = ('status', 'editor', 'phone_number', 'question')
+    search_fields = ('editor', 'phone_number', 'question')
+    list_filter = ('status',)
     ordering = ['-update_date']
-
-    exclude = ('tg_user', 'vk_user',)
 
     def get_fieldsets(self, *args, **kwargs):
         fieldsets = super(RequestHistoryAdmin, self).get_fieldsets(*args, **kwargs)
